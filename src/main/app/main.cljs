7fb97bfc1055bb1a05aea7a5beb7551c949ad6a9
Phuoc Binh Ha <phuocbinhha310@gmail.com>
2022-05-14 19:03:29 +0700

    add Paper components

--- src/main/app/main.cljs
+++ src/main/app/main.cljs
@@ -1,11 +1,10 @@
 (ns app.main
   (:require
-    [shadow.react-native :refer (render-root)]
-    ["react-native" :as rn]
-    [reagent.core :as r]
-    ["buffer" :refer (Buffer)]
-    ["react-native-image-picker" :refer (launchImageLibrary)]
-    ["jpeg-js" :as jpeg]))
+   [shadow.react-native :refer (render-root)]
+   ["react-native" :as rn]
+   [reagent.core :as r]
+   ["react-native-paper" :refer (DarkTheme Appbar Provider List)]
+   ["buffer" :refer (Buffer)]))
 
 ;; must use defonce and must refresh full app so metro can fill these in
 ;; at live-reload time `require` does not exist and will cause errors
@@ -15,13 +14,19 @@
 (def styles
   ^js (-> {:container
            {:flex 1
-            :backgroundColor "#fff"
-            :alignItems "center"
-            :justifyContent "center"}
+            :backgroundColor "#fff"}
            :title
            {:fontWeight "bold"
             :fontSize 24
-            :color "blue"}}
+            :color "blue"}
+           :appbar
+           {:position "absolute"
+            :left 0
+            :right 0
+            :top 0}
+           :list
+           {:flex 1
+            :marginTop 50}}
           (clj->js)
           (rn/StyleSheet.create)))
 
@@ -32,35 +37,30 @@
        [:> rn/Text "atom's current value: " @count ". "]
        [:> rn/Button {:on-press #(swap! count inc) :title "Increase"}]])))
 
-(defn picker-callback [args]
-  (let [a (js->clj args :keywordize-keys true)
-        didCancel (:didCancel a)
-        errorCode (:errorCode a)
-        assets (:assets a)
-        fileSize (:fileSize a)]
-    (if (or (some? didCancel) errorCode (nil? assets) (= 0 fileSize))
-      (println "Some error occur: " errorCode)
-      (letfn [(extract-buffer [assets]
-                (-> assets
-                (nth 0)
-                :base64
-                (#(.from Buffer % "base64"))
-                (#(.decode jpeg % #js {:useTArray true}))
-                .-data))]
-        (let [buffer (extract-buffer assets)]
-          (println buffer)
-          buffer)))))
+(defn app-bar []
+  [:> Appbar {:style (.-appbar styles)}])
 
-(defn picker []
-  (let [options #js {:selectionLimit 1
-                     :mediaType "photo"
-                     :includeBase64 true}]
-    (launchImageLibrary options picker-callback)))
+(defn sub-list [title]
+  (fn []
+    [:> (.-Section List) {:style (.-list styles)}
+     [:> (.-Subheader List) title]
+     [:> (.-Item List) {:title "First item"}]
+     [:> (.-Item List) {:title "First item"}]
+     [:> (.-Item List) {:title "First item"}]
+     [:> (.-Item List) {:title "First item"}]
+     [:> (.-Item List) {:title "First item"}]]))
+
+(defn app []
+  [:> rn/View {:style (.-container styles)}
+   [:> rn/View {:style (.-containter styles)}
+    [app-bar]]
+   [sub-list "List"]])
+   ;;[:> rn/Text {:style (.-title styles)} "Hello!"]
+   ;;[:> rn/Image {:source splash-img :style {:width 200 :height 200}}]])
 
 (defn root []
-  [:> rn/View {:style (.-container styles)}
-   [:> rn/Text {:style (.-title styles)} "Hello!"]
-   [:> rn/Image {:source splash-img :style {:width 200 :height 200}}]])
+  [:> Provider
+   [app]])
 
 (defn start
   {:dev/after-load true}
