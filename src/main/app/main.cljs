f8b705e2ab1eb28b5396775f34a9c5233a507424
Phuoc Binh Ha <phuocbinhha310@gmail.com>
2022-05-15 13:26:29 +0700

    add deps.edn dependencies

--- src/main/app/main.cljs
+++ src/main/app/main.cljs
@@ -3,7 +3,9 @@
    [shadow.react-native :refer (render-root)]
    ["react-native" :as rn]
    [reagent.core :as r]
+   ["react-native-calendars" :refer (Calendar)]
    ["react-native-paper" :refer (DarkTheme Appbar Provider List)]
+   [tick.alpha.interval :as t.i]
    ["buffer" :refer (Buffer)]))
 
 ;; must use defonce and must refresh full app so metro can fill these in
@@ -50,13 +52,29 @@
      [:> (.-Item List) {:title "First item"}]
      [:> (.-Item List) {:title "First item"}]]))
 
+(defn calendar []
+  [:> Calendar {:marked-dates (clj->js {:2022-05-16 {:marked true}})
+                :on-day-press (fn [day] (println day))}])
+
 (defn app []
   [:> rn/View {:style (.-container styles)}
    [:> rn/View {:style (.-containter styles)}
     [app-bar]]
+   [:> Calendar {:on-day-press (fn [day] (println day))
+                 :first-day 1
+                 :marking-type "multi-period"
+                 ;;:on-month-change (fn [month] (println month))
+                 :marked-dates (clj->js {:2022-05-14 {:periods [{:startingDay false :endingDay true :color "#5f9ea0"}
+                                                               {:startingDay false :endingDay true :color "#ffa500"}
+                                                               {:startingDay true :endingDay false :color "#f0e68c"}]}
+                                         :2022-05-15 {:periods [{:color "transparent"} {:color "transparent"} {:startingDay false :endingDay false :color "#f0e68c"}]}
+                                         :2022-05-16 {:periods [{:startingDay true :endingDay false :color "#ffa500"}
+                                                               {:color "transparent"}
+                                                               {:startingDay false :endingDay false :color "#f0e68c"}]}})}]
+   ;;[calendar]
    [sub-list "List"]])
-   ;;[:> rn/Text {:style (.-title styles)} "Hello!"]
-   ;;[:> rn/Image {:source splash-img :style {:width 200 :height 200}}]])
+;;[:> rn/Text {:style (.-title styles)} "Hello!"]
+;;[:> rn/Image {:source splash-img :style {:width 200 :height 200}}]])
 
 (defn root []
   [:> Provider
